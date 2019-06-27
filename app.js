99b72d9f0484dd31a3c487a5abc28294b3fb39a6
sarad <gyawalisarad@gmail.com>
2019-06-26 18:27:17 -0700

    port 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const bodyParser = require("body-parser");
-const port = process.env.PORT || 22;
+const port = process.env.PORT || 80;
 let averageFeeInDollar;
 let rewardToken;
 
